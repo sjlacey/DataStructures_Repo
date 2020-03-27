f9043d13b70b99459d5c8a2dab996c21701ddf20
sjlacey <seanjlacey@outlook.com>
2020-03-27 16:23:11 +0000

    HeapPriorityQueue and PQSort (for assignment 1) fully implemented

--- src/projectCode20280/HeapPriorityQueue.java
+++ src/projectCode20280/HeapPriorityQueue.java
@@ -10,18 +10,32 @@ import java.util.Comparator;
  * An implementation of a priority queue using an array-based heap.
  */
 
-public class HeapPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
+public class HeapPriorityQueue<K,V> extends AbstractPriorityQueue<K,V>
+{
 
-	/** Creates an empty priority queue based on the natural ordering of its keys. */
-  public HeapPriorityQueue() { super(); }
+  protected ArrayList<Entry<K,V>> heap = new ArrayList<>();
 
-  /**
+
+/** Creates an empty priority queue based on the natural ordering of its keys. */
+
+  public HeapPriorityQueue()
+  {
+    super();
+  }
+
+
+/**
    * Creates an empty priority queue using the given comparator to order keys.
    * @param comp comparator defining the order of keys in the priority queue
    */
-  public HeapPriorityQueue(Comparator<K> comp) { super(comp); }
 
-  /**
+  public HeapPriorityQueue(Comparator<K> comp)
+  {
+    super(comp);
+  }
+
+
+/**
    * Creates a priority queue initialized with the respective
    * key-value pairs.  The two arrays given will be paired
    * element-by-element. They are presumed to have the same
@@ -30,63 +44,161 @@ public class HeapPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
    * @param keys an array of the initial keys for the priority queue
    * @param values an array of the initial values for the priority queue
    */
-  public HeapPriorityQueue(K[] keys, V[] values) ;
+
+  public HeapPriorityQueue(K[] keys, V[] values)
+  {
+    super();
+    for(int j=0; j < Math.min(keys.length, values.length); j++)
+    {
+      heap.add(new PQEntry<>(keys[j], values[j]));
+    }
+    heapify();
+  }
 
   // protected utilities
-  protected int parent(int j);
-  protected int left(int j) ;
-  protected int right(int j) ;
-  protected boolean hasLeft(int j);
-  protected boolean hasRight(int j);
+  protected int parent(int j)
+  {
+    return j/2;
+  }
+  protected int left(int j)
+  {
+    return j*2;
+  }
+  protected int right(int j)
+  {
+    return j*2 + 1;
+  }
+  protected boolean hasLeft(int j)
+  {
+    return left(j) < heap.size();
+  }
+  protected boolean hasRight(int j)
+  {
+    return right(j) < heap.size();
+  }
 
-  /** Exchanges the entries at indices i and j of the array list. */
-  protected void swap(int i, int j) ;
+/** Exchanges the entries at indices i and j of the array list. */
 
-  /** Moves the entry at index j higher, if necessary, to restore the heap property. */
-  protected void upheap(int j);
-  
-  /** Moves the entry at index j lower, if necessary, to restore the heap property. */
-  protected void downheap(int j) ;
+  protected void swap(int i, int j)
+  {
+    Entry<K,V> temp = heap.get(i);
+    heap.set(i, heap.get(j));
+    heap.set(j, temp);
+  }
 
-  /** Performs a bottom-up construction of the heap in linear time. */
-  protected void heapify() ;
+/** Moves the entry at index j higher, if necessary, to restore the heap property. */
+
+  protected void upheap(int j)
+  {
+    while(j>0) //continue until we reach the root, or the break
+    {
+      int p = parent(j);
+      if(compare(heap.get(j), heap.get(p)) >= 0) break; //heap property verified!
+      swap(j, p);
+      j=p; //continue from the parent's location
+    }
+  }
+
+/** Moves the entry at index j lower, if necessary, to restore the heap property. */
+
+  protected void downheap(int j)
+  {
+    while(hasLeft(j)) //continue to bottom, or to the break statement
+    {
+      int leftIndex = left(j);
+      int smallChildIndex = leftIndex; //but what if the right is smaller? if statements required:
+      if(hasRight(j))
+      {
+        int rightIndex = right(j);
+        if(compare(heap.get(leftIndex), heap.get(rightIndex)) > 0)
+        {
+          smallChildIndex = rightIndex; //right child IS, in fact, smaller
+        }
+      }
+      if(compare(heap.get(smallChildIndex), heap.get(j)) >= 0)
+        break;
+
+      swap(j, smallChildIndex); //heap property has been restored
+      j = smallChildIndex;      //continue at the position of the child
+    }
+  }
+
+/** Performs a bottom-up construction of the heap in linear time. */
+
+  protected void heapify()
+  {
+    int startIndex = parent(size()-1);
+    for(int j= startIndex; j>=0; j--)
+    {
+      downheap(j);
+    }
+  }
 
   // public methods
-
-  /**
+/**
    * Returns the number of items in the priority queue.
    * @return number of items
    */
-  @Override
-  public int size() { return heap.size(); }
 
-  /**
+  @Override
+  public int size()
+  {
+    return heap.size();
+  }
+
+/**
    * Returns (but does not remove) an entry with minimal key.
    * @return entry having a minimal key (or null if empty)
    */
-  @Override
-  public Entry<K,V> min() ;
 
-  /**
+  @Override
+  public Entry<K,V> min()
+  {
+    if(heap.isEmpty())
+      return null;
+
+    return heap.get(0);
+  }
+
+/**
    * Inserts a key-value pair and return the entry created.
    * @param key     the key of the new entry
    * @param value   the associated value of the new entry
    * @return the entry storing the new key-value pair
    * @throws IllegalArgumentException if the key is unacceptable for this queue
    */
-  @Override
-  public Entry<K,V> insert(K key, V value) throws IllegalArgumentException ;
 
-  /**
+  @Override
+  public Entry<K,V> insert(K key, V value) throws IllegalArgumentException
+  {
+    checkKey(key); //auxiliary key-checking method (could throw exception)
+    Entry<K,V> newest = new PQEntry<>(key, value);
+    heap.add(newest);         //adds to the end of the list
+    upheap(heap.size()-1); //upheap the newly added entry
+
+    return newest;
+  }
+/**
    * Removes and returns an entry with minimal key.
    * @return the removed entry (or null if empty)
    */
-  @Override
-  public Entry<K,V> removeMin() ;
 
-  /** Used for debugging purposes only */
-  private void sanityCheck() {
-    for (int j=0; j < heap.size(); j++) {
+  @Override
+  public Entry<K,V> removeMin()
+  {
+    if(heap.isEmpty()) return null;
+    Entry<K,V> answer = heap.get(0);
+    swap(0, heap.size()-1);    //put minimum item at the end
+    heap.remove(heap.size()-1);      //and remove it from the list,
+    downheap(0);                  //then fix the new root!
+    return answer;
+  }
+/** Used for debugging purposes only */
+
+  private void sanityCheck()
+  {
+    for (int j=0; j < heap.size(); j++)
+    {
       int left = left(j);
       int right = right(j);
       if (left < heap.size() && compare(heap.get(left), heap.get(j)) < 0)
@@ -96,4 +208,3 @@ public class HeapPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
     }
   }
 }
-
--- src/projectCode20280/LinkedBinaryTree.java
+++ src/projectCode20280/LinkedBinaryTree.java
@@ -1,20 +1,25 @@
+/*
 package projectCode20280;
 
-/**
+*
  * Concrete implementation of a binary tree using a node-based, linked structure.
- */
+
+
 public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTree<E>
 {
 
   //LinkedBinaryTree instance variables
-  /** The root of the binary tree */
+* The root of the binary tree
+
   protected Node<E> root = null;     // root of the tree
 
-  /** The number of nodes in the binary tree */
+* The number of nodes in the binary tree
+
   private int size = 0;              // number of nodes in the tree
 
 
-  /** Nested static class for a binary tree node. */
+* Nested static class for a binary tree node.
+
   protected static class Node<E> implements Position<E>
   {
     private E element;
@@ -44,18 +49,20 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
 
   }
 
-  /** Factory function to create a new node storing element e. */
+* Factory function to create a new node storing element e.
+
   protected Node<E> createNode(E e, Node<E> parent, Node<E> left, Node<E> right)
   {
     return new Node<E>(e, parent, left, right);
   }
 
   // constructor
-  /** Construts an empty binary tree. */
+* Constructs an empty binary tree.
+
   public LinkedBinaryTree() { }      // constructs an empty binary tree
 
   // nonpublic utility
-  /**
+*
    * Verifies that a Position belongs to the appropriate class, and is
    * not one that has been previously removed. Note that our current
    * implementation does not actually verify that the position belongs
@@ -64,7 +71,8 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
    * @param p   a Position (that should belong to this tree)
    * @return    the underlying Node instance for the position
    * @throws IllegalArgumentException if an invalid position is detected
-   */
+
+
   protected Node<E> validate(Position<E> p) throws IllegalArgumentException
   {
     if (!(p instanceof Node))
@@ -76,69 +84,92 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
   }
 
   // accessor methods (not already implemented in AbstractBinaryTree)
-  /**
+*
    * Returns the number of nodes in the tree.
    * @return number of nodes in the tree
-   */
+
+
   @Override
   public int size() {
     return size;
   }
 
-  /**
+*
    * Returns the root Position of the tree (or null if tree is empty).
    * @return root Position of the tree (or null if tree is empty)
-   */
+
+
   @Override
   public Position<E> root() {
     return root;
   }
 
-  /**
+*
    * Returns the Position of p's parent (or null if p is root).
    *
    * @param p    A valid Position within the tree
    * @return Position of p's parent (or null if p is root)
    * @throws IllegalArgumentException if p is not a valid Position for this tree.
-   */
-  /*@Override
+
+
+  @Override
   public Position<E> parent(Position<E> p) throws IllegalArgumentException
   {
-    //TODO, see description above
-  }*/
+    Node<E> node = validate(p);
+    if(node == root)
+    {
+      throw new IllegalArgumentException("The node has no parent as it is the root.");
+    }
+    else return ((Node<E>) p).parent;
+  }
 
-  /**
+*
    * Returns the Position of p's left child (or null if no child exists).
    *
    * @param p A valid Position within the tree
    * @return the Position of the left child (or null if no child exists)
    * @throws IllegalArgumentException if p is not a valid Position for this tree
-   */
+
+
   @Override
-  public Position<E> left(Position<E> p) throws IllegalArgumentException {
-    //TODO
+  public Position<E> left(Position<E> p) throws IllegalArgumentException
+  {
+    Node<E> node = validate(p);
+    if(node.getLeft() == null)
+    {
+      throw new IllegalArgumentException("This node has no left sibling.");
+    }
+    else return node.getLeft();
   }
 
-  /**
+*
    * Returns the Position of p's right child (or null if no child exists).
    *
    * @param p A valid Position within the tree
    * @return the Position of the right child (or null if no child exists)
    * @throws IllegalArgumentException if p is not a valid Position for this tree
-   */
+
+
   @Override
-  public Position<E> right(Position<E> p) throws IllegalArgumentException {
-    //TODO
+  public Position<E> right(Position<E> p) throws IllegalArgumentException
+  {
+    Node<E> node = validate(p);
+    if(node.getRight() == null)
+    {
+      throw new IllegalArgumentException("This node has no right sibling.");
+    }
+    else return node.getRight();
   }
 
   // update methods supported by this class
-  /**
+*
    * Places element e at the root of an empty tree and returns its new Position.
    *
    * @param e   the new element
    * @return the Position of the new element
    * @throws IllegalStateException if the tree is not empty
-   */
+
+
   public Position<E> addRoot(E e) throws IllegalStateException
   {
     if(!isEmpty()) throw new IllegalStateException("Tree is not empty! Cannot add a root.");
@@ -146,19 +177,24 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
     return root = createNode(e,null, null, null);
   }
 
-  public void insert(E e){
+  public void insert(E e)
+  {
     //recursively add from root
     root = addRecursive(root, e);
     ++size;
   }
 
   //recursively add Nodes to binary tree in proper position
-  private Node<E> addRecursive(Node<E> p, E e){
-    //TODO
+  private Node<E> addRecursive(Node<E> p, E e)
+  {
+    if(p.left==null && p.right==null)
+    {
+      return createNode(e, p.parent, null, null);
+    }
+    return p;
   }
 
-
-  /**
+*
    * Creates a new left child of Position p storing element e and returns its Position.
    *
    * @param p   the Position to the left of which the new element is inserted
@@ -166,7 +202,8 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
    * @return the Position of the new element
    * @throws IllegalArgumentException if p is not a valid Position for this tree
    * @throws IllegalArgumentException if p already has a left child
-   */
+
+
   public Position<E> addLeft(Position<E> p, E e) throws IllegalArgumentException
   {
     Node<E> parent = validate(p);
@@ -177,7 +214,7 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
     return child;
   }
 
-  /**
+*
    * Creates a new right child of Position p storing element e and returns its Position.
    *
    * @param p   the Position to the right of which the new element is inserted
@@ -185,7 +222,8 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
    * @return the Position of the new element
    * @throws IllegalArgumentException if p is not a valid Position for this tree.
    * @throws IllegalArgumentException if p already has a right child
-   */
+
+
   public Position<E> addRight(Position<E> p, E e) throws IllegalArgumentException
   {
     Node<E> parent = validate(p);
@@ -196,19 +234,26 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
     return child;
   }
 
-  /**
+*
    * Replaces the element at Position p with element e and returns the replaced element.
    *
    * @param p   the relevant Position
    * @param e   the new element
    * @return the replaced element
    * @throws IllegalArgumentException if p is not a valid Position for this tree.
-   */
-  public E set(Position<E> p, E e) throws IllegalArgumentException {
-    //TODO
+
+
+  public E set(Position<E> p, E e) throws IllegalArgumentException
+  {
+    E returnElement = p.getElement();
+    Node<E> node = validate(p);
+
+    node.setElement(e);
+
+    return returnElement;
   }
 
-  /**
+*
    * Attaches trees t1 and t2, respectively, as the left and right subtree of the
    * leaf Position p. As a side effect, t1 and t2 are set to empty trees.
    *
@@ -217,20 +262,31 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
    * @param t2  an independent tree whose structure becomes the right child of p
    * @throws IllegalArgumentException if p is not a valid Position for this tree
    * @throws IllegalArgumentException if p is not a leaf
-   */
+
+
   public void attach(Position<E> p, LinkedBinaryTree<E> t1,
-                     LinkedBinaryTree<E> t2) throws IllegalArgumentException {
+                     LinkedBinaryTree<E> t2) throws IllegalArgumentException
+  {
+    Node<E> node = validate(p);
+
+    if(node.getLeft()!=null || node.getRight()!=null)
+    {
+      throw new IllegalArgumentException("p is not a leaf");
+    }
     //TODO
+    //node.setLeft(t1);
+
   }
 
-  /**
+*
    * Removes the node at Position p and replaces it with its child, if any.
    *
    * @param p   the relevant Position
    * @return element that was removed
    * @throws IllegalArgumentException if p is not a valid Position for this tree.
    * @throws IllegalArgumentException if p has two children.
-   */
+
+
   public E remove(Position<E> p) throws IllegalArgumentException {
     //TODO
   }
@@ -247,7 +303,7 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
     return sb.toString();
   }
 
-  /*public static void main(String [] args) {
+  public static void main(String [] args) {
     LinkedBinaryTree<Integer> bt = new LinkedBinaryTree<Integer>();
 
     int [] arr = {12, 25, 31, 58, 36, 42, 90, 62, 75};
@@ -256,6 +312,7 @@ public class LinkedBinaryTree<E extends Comparable<E>> extends AbstractBinaryTre
     }
     System.out.println("bt: " + bt.size() + " " + bt );
 
-  }*/
+  }
 }
 
+*/
