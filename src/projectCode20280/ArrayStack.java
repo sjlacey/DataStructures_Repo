5db679207852125149de279a4bb090e5e08b3149
sjlacey <seanjlacey@outlook.com>
2020-03-28 17:38:27 +0000

    Array based stack fully implemented.

--- src/projectCode20280/ArrayStack.java
+++ src/projectCode20280/ArrayStack.java
@@ -1,40 +1,92 @@
 package projectCode20280;
 
-public class ArrayStack<E> implements Stack<E> {
+public class ArrayStack<E> implements Stack<E>
+{
+	public static final int CAPACITY = 1000;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private E[] data;
 
+	private int size=0;
+
+	public ArrayStack()
+	{
+		this(CAPACITY); //constructs stack with default capacity - notes
+	}
+
+	@SuppressWarnings({"unchecked"})
+	public ArrayStack(int capacity) //constructs stack with given capacity - notes
+	{
+		data = (E[]) new Object[capacity]; //safe cast; compiler may give warning - notes
 	}
 
 	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int size()
+	{
+		return size;
 	}
 
 	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isEmpty()
+	{
+		return size==0;
 	}
 
 	@Override
-	public void push(E e) {
-		// TODO Auto-generated method stub
-		
+	public void push(E e)
+	{
+		data[size] = e;
+		size++;
 	}
 
 	@Override
-	public E top() {
-		// TODO Auto-generated method stub
-		return null;
+	public E top()
+	{
+		return data[size-1];
 	}
 
 	@Override
-	public E pop() {
-		// TODO Auto-generated method stub
-		return null;
+	public E pop()
+	{
+		if(isEmpty())
+		{
+			return null;
+		}
+
+		E temp = top();
+
+		data[size-1] = null;
+
+		size--;
+		return temp;
+	}
+
+	@Override
+	public String toString()
+	{
+		StringBuilder string = new StringBuilder();
+
+		for(int i=0; i<size; i++)
+		{
+			string.append(data[i]).append("  ");
+		}
+
+		return string.toString();
+	}
+
+	public static void main(String[] args)
+	{
+		ArrayStack AS = new ArrayStack();
+
+		AS.push(1);
+		AS.push(2);
+		AS.push(3);
+		AS.push(4);
+		AS.push(5);
+		System.out.println("Initial array: " + AS.toString());
+		AS.pop();
+		AS.pop();
+		System.out.println("Size: " + AS.size());
+		System.out.println("Array after two pops: " + AS.toString());
 	}
 
 }
